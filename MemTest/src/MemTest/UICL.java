7606832663ba73531dedfb19f14b41d466cff33e
p0sE <pose@gmx.net>
2016-12-01 17:01:42 +0100

    initial setup

---
+++ MemTest/src/MemTest/UICL.java
@@ -0,0 +1,19 @@
+package MemTest;
+
+public class UICL {
+	
+	// URL & setup data
+	private static final String START_PAGE_URL = "about:blank";
+	
+	public UICL() {
+
+		int number = 0;
+		String name = new String();
+
+		
+
+	}
+	
+
+
+}
